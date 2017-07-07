60018d9b4b72c5e17ffe73f9e32af967d0c91012
Joshua <jmg1138@gmail.com>
2017-07-07 14:20:58 -0700

    Added cors

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+'use strict'
+
 /**
  * The trackerTimer.
  * @namespace trackertimer
@@ -7,59 +9,50 @@
  */
 
 /**
- * Invoke strict mode for the entire script.
- * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode Strict mode}
- */
-"use strict";
-
-/**
  * Require the 3rd party modules that will be used.
  * @see {@link https://github.com/expressjs/express Express}
  * @see {@link https://github.com/helmetjs Helmet}
  */
-const express = require("express");
-const helmet = require("helmet");
+const cors = require('cors')
+const express = require('express')
+const helmet = require('helmet')
 
 /**
  * Require the local modules that will be used.
  */
-const routes = require("./routes.js");
+const routes = require('./routes.js')
 
 /**
  * Define the port for the application entry point to listen on.
  * Use port 1138 if environmental variable PORT is not defined.
  * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt MDN JavaScript parseInt}
  */
-const port = parseInt(process.env.PORT, 10) || 1138;
+const port = parseInt(process.env.PORT, 10) || 1138
 
 /**
  * Define all app configurations here except routes (define routes last).
  * Instantiate the Express application.
  */
-const app = express();
-app.use(helmet());
-app.set('json spaces', 2);
+const app = express()
+app.use(helmet())
+app.use(cors())
+app.set('json spaces', 2)
 
 /**
  * Define routes last, after all other configurations.
  * @param {object} app - The Express application instance.
  */
-routes(app);
+routes(app)
 
 /**
  * Listen for connections on the specified port.
  * @see {@link https://expressjs.com/en/api.html#app.listen Express API app.listen}
  */
 app.listen(parseInt(process.env.PORT, 10) || 1138, function () {
-    console.log("trackerTimer API listening.");
+  console.log('trackerTimer API listening.')
 }).on('error', function (err) {
-    console.log(err);
-    // TODO: If error, try again a number of times and then give up.
-});
+  console.log(err)
+  // TODO: If error, try again a number of times and then give up.
+})
 
-/**
- * Assign our app object to module.exports.
- * @see {@link https://nodejs.org/api/modules.html#modules_the_module_object Nodejs modules: The module object}
- * @see {@link https://nodejs.org/api/modules.html#modules_module_exports Nodejs modules: module exports}
- */
-module.exports = app; // For testing with supertest
+module.exports = app // For testing with supertest
