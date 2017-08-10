bb9303a7bdfd48f4330597358ba511e1a4122d95
Joshua <jmg1138@gmail.com>
2017-08-10 10:03:05 -0700

    Added package-lock.json

--- app.js
+++ app.js
@@ -3,56 +3,36 @@
 'use strict'
 
 /**
- * The trackerTimer.
- * @namespace trackertimer
- * @author jmg1138 {@link https://github.com/jmg1138 jmg1138 on GitHub}
+ * trackerTimer API.
+ * @author jmg1138 {@link https://github.com/jmg1138 jmg1138}
  */
 
 /**
- * Require the 3rd party modules that will be used.
+ * Require the modules that will be used.
+ * @see {@link https://github.com/expressjs/cors cors}
  * @see {@link https://github.com/expressjs/express Express}
  * @see {@link https://github.com/helmetjs Helmet}
  */
 const cors = require('cors')
 const express = require('express')
 const helmet = require('helmet')
-
-/**
- * Require the local modules that will be used.
- */
 const routes = require('./routes.js')
 
 /**
- * Define the port for the application entry point to listen on.
- * Use port 1138 if environmental variable PORT is not defined.
- * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt MDN JavaScript parseInt}
- */
-const port = parseInt(process.env.PORT, 10) || 1138
-
-/**
- * Define all app configurations here except routes (define routes last).
- * Instantiate the Express application.
+ * Instantiate the Express application, define all app configurations here, and
+ * then define routes last.
  */
 const app = express()
 app.use(helmet())
 app.use(cors())
 app.set('json spaces', 2)
-
-/**
- * Define routes last, after all other configurations.
- * @param {object} app - The Express application instance.
- */
 routes(app)
 
 /**
  * Listen for connections on the specified port.
- * @see {@link https://expressjs.com/en/api.html#app.listen Express API app.listen}
  */
-app.listen(parseInt(process.env.PORT, 10) || 1138, function () {
-  console.log('trackerTimer API listening.')
-}).on('error', function (err) {
-  console.log(err)
-  // TODO: If error, try again a number of times and then give up.
-})
+const port = parseInt(process.env.PORT, 10) || 1138
+app.listen(port, () => console.log(`Listening on port ${port}.`))
+app.on('error', err => console.log(err.message))
 
-module.exports = app // For testing with supertest
+module.exports = app // For supertest
